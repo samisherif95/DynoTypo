bdee6e8b4e95e0e2a4977c5ac9458aee48c1571b
Sami Ellaboudy <sami.sherif95@gmail.com>
2020-02-02 16:51:12 -0800

    debugging

--- src/dino.js
+++ src/dino.js
@@ -7,7 +7,7 @@ class Dino{
         this.word = word
         this.alive = alive
         this.DinoImage = new Image();
-        this.DinoImage.src = 'https://github.com/samisherif95/DynoTypo/blob/master/dist/public/images/DinoSprite.png'
+        this.DinoImage.src = '/Users/samisherif/Desktop/DynoTypo/dist/public/images/DinoSprite.png'
         this.srcX = 0;
         this.srcY = 0;
         this.moveX = 10;
